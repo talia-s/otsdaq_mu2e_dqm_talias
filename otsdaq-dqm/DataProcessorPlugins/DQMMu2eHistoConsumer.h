d486fb8d5269fa2b324b92064c3cc1872d24b8fc
Greg <grakness@FNAL.GOV>
2020-03-16 17:57:23 -0500

    Aggregator checkpoint with visualizer more stable

--- otsdaq-dqm/DataProcessorPlugins/DQMMu2eHistoConsumer.h
+++ otsdaq-dqm/DataProcessorPlugins/DQMMu2eHistoConsumer.h
@@ -8,37 +8,39 @@
 class TFile;
 class TDirectory;
 class TH1F;
+class TTree;
 
 namespace ots
 {
 
-class DQMMu2eHistoConsumer : public DQMHistosConsumerBase, public Configurable
-{
-public:
-  DQMMu2eHistoConsumer(std::string supervisorApplicationUID, std::string bufferUID, std::string processorUID, const ConfigurationTree& theXDAQContextConfigTree, const std::string& configurationPath);
-	virtual ~DQMMu2eHistoConsumer(void);
+	class DQMMu2eHistoConsumer : public DQMHistosConsumerBase, public Configurable
+	{
+		public:
+	  		DQMMu2eHistoConsumer(std::string supervisorApplicationUID, std::string bufferUID, std::string processorUID, const ConfigurationTree& theXDAQContextConfigTree, const std::string& configurationPath);
+			virtual ~DQMMu2eHistoConsumer(void);
 
-	void startProcessingData (std::string runNumber) override;
-	void stopProcessingData  (void) override;
-	void pauseProcessingData (void) override;
-	void resumeProcessingData(void) override;
-	void load                (std::string fileName){;}
+			void startProcessingData (std::string runNumber) override;
+			void stopProcessingData  (void) override;
+			void pauseProcessingData (void) override;
+			void resumeProcessingData(void) override;
+			void load                (std::string fileName){;}
 
-private:
-	bool workLoopThread(toolbox::task::WorkLoop* workLoop);
-	void fastRead(void);
-	void slowRead(void);
+		private:
+			bool workLoopThread(toolbox::task::WorkLoop* workLoop);
+			void fastRead(void);
+			void slowRead(void);
 	
-	//For fast read
-	std::string*                       dataP_;
-	std::map<std::string,std::string>* headerP_;
+			//For fast read
+			std::string*                       dataP_;
+			std::map<std::string,std::string>* headerP_;
 
-	bool                               saveFile_; //yes or no
-	std::string                        filePath_;
-	std::string                        radixFileName_;
+			bool                               saveFile_; //yes or no
+			std::string                        filePath_;
+			std::string                        radixFileName_;
 
-	TH1F*                              hGauss_;
-};
+			TH1F*                              hGauss_;//TODO need to add our own class here
+			TTree*                             testTree_;
+	};
 }
 
 #endif
--- otsdaq-dqm/DataProcessorPlugins/DQMMu2eHistoConsumer_processor.cc
+++ otsdaq-dqm/DataProcessorPlugins/DQMMu2eHistoConsumer_processor.cc
@@ -7,84 +7,101 @@
 #include <TDirectory.h>
 #include <TFile.h>
 #include <TH1F.h>
+#include <TTree.h>
 
 using namespace ots;
 
 //========================================================================================================================
-DQMMu2eHistoConsumer::DQMMu2eHistoConsumer(std::string supervisorApplicationUID, std::string bufferUID, std::string processorUID, const ConfigurationTree& theXDAQContextConfigTree, const std::string& configurationPath)
-: WorkLoop             (processorUID)
-, DQMHistosConsumerBase(supervisorApplicationUID, bufferUID, processorUID, LowConsumerPriority)
-, Configurable         (theXDAQContextConfigTree, configurationPath)
-, saveFile_            (theXDAQContextConfigTree.getNode(configurationPath).getNode("SaveFile").getValue<bool>())
-, filePath_            (theXDAQContextConfigTree.getNode(configurationPath).getNode("FilePath").getValue<std::string>())
-, radixFileName_       (theXDAQContextConfigTree.getNode(configurationPath).getNode("RadixFileName").getValue<std::string>())
+	DQMMu2eHistoConsumer::DQMMu2eHistoConsumer(std::string supervisorApplicationUID, std::string bufferUID, std::string processorUID, const 		ConfigurationTree& theXDAQContextConfigTree, const std::string& configurationPath)
+		: WorkLoop             (processorUID)
+		, DQMHistosConsumerBase(supervisorApplicationUID, bufferUID, processorUID, LowConsumerPriority)
+		, Configurable         (theXDAQContextConfigTree, configurationPath)
+		, saveFile_            (theXDAQContextConfigTree.getNode(configurationPath).getNode("SaveFile").getValue<bool>())
+		, filePath_            (theXDAQContextConfigTree.getNode(configurationPath).getNode("FilePath").getValue<std::string>())
+		, radixFileName_       (theXDAQContextConfigTree.getNode(configurationPath).getNode("RadixFileName").getValue<std::string>())
 
-{
-}
-
-//========================================================================================================================
-DQMMu2eHistoConsumer::~DQMMu2eHistoConsumer(void)
-{
-	DQMHistosBase::closeFile();
-}
-//========================================================================================================================
-void DQMMu2eHistoConsumer::startProcessingData(std::string runNumber)
-{
-	DQMHistosBase::openFile(filePath_ + radixFileName_ + runNumber + ".root");
-	DQMHistosBase::myDirectory_ = DQMHistosBase::theFile_->mkdir("Mu2eHistos", "Mu2eHistos");
-	DQMHistosBase::myDirectory_->cd();
-	hGauss_ = new TH1F("Gaussian", "Gaussian", 100, 0, 100);
-
-	std::cout << __PRETTY_FUNCTION__ << "Starting!" << std::endl;
-	DataConsumer::startProcessingData(runNumber);
-	std::cout << __PRETTY_FUNCTION__ << "Started!" << std::endl;
-}
-
-//========================================================================================================================
-void DQMMu2eHistoConsumer::stopProcessingData(void)
-{
-	DataConsumer::stopProcessingData();
-	if(saveFile_)
 	{
-		DQMHistosBase::save();
+		std::cout<<"[In DQMMu2eHistoConsumer () ] Initiating ..."<<std::endl;
 	}
-	closeFile();
-}
 
 //========================================================================================================================
-void DQMMu2eHistoConsumer::pauseProcessingData(void)
-{
-	DataConsumer::stopProcessingData();
-}
-
-//========================================================================================================================
-void DQMMu2eHistoConsumer::resumeProcessingData(void)
-{
-	DataConsumer::startProcessingData("");
-}
-
-//========================================================================================================================
-bool DQMMu2eHistoConsumer::workLoopThread(toolbox::task::WorkLoop* workLoop)
-{
-	//std::cout << __COUT_HDR_FL__ << __PRETTY_FUNCTION__ << DataProcessor::processorUID_ << " running, because workloop: " << WorkLoop::continueWorkLoop_ << std::endl;
-	fastRead();
-	return WorkLoop::continueWorkLoop_;
-}
-
-//========================================================================================================================
-void DQMMu2eHistoConsumer::fastRead(void)
-{
-	if(DataConsumer::read(dataP_, headerP_) < 0)
+	DQMMu2eHistoConsumer::~DQMMu2eHistoConsumer(void)
 	{
-		std::this_thread::sleep_for(std::chrono::milliseconds(10));
-		return;
+		DQMHistosBase::closeFile();
 	}
-	double value = 0;
-	memcpy(&value, &dataP_->at(0), sizeof(double));
-//	std::cout << __PRETTY_FUNCTION__ << " Value: " << value << std::endl;
+//========================================================================================================================
+	void DQMMu2eHistoConsumer::startProcessingData(std::string runNumber)
+		{
+		  std::cout << __PRETTY_FUNCTION__ << filePath_ + "/" + radixFileName_ + "_Run" + runNumber + ".root" << std::endl;
+			DQMHistosBase::openFile(filePath_ + "/" + radixFileName_ + "_Run" + runNumber + ".root");//filePath_ + radixFileName_
+			DQMHistosBase::myDirectory_ = DQMHistosBase::theFile_->mkdir("Mu2eHistos", "Mu2eHistos");
+			DQMHistosBase::myDirectory_->cd();
+			DQMHistosBase::myDirectory_->mkdir("ExtraDir", "ExtraDir");
+			hGauss_ = new TH1F("Gaussian", "Gaussian", 100, 0, 100);
+			testTree_ = new TTree("tree", "tree");
+			int value;
+			testTree_->Branch("Value", &value);
+			value = 7;
+			testTree_->Fill();
+			value = 8;
+			testTree_->Fill();
 
-	hGauss_->Fill(value);
-	DataConsumer::setReadSubBuffer<std::string, std::map<std::string, std::string>>();
-}
+			std::cout << __PRETTY_FUNCTION__ << "Starting!" << std::endl;
+			DataConsumer::startProcessingData(runNumber);
+			std::cout << __PRETTY_FUNCTION__ << "Started!" << std::endl;
+		}
+
+//========================================================================================================================
+	void DQMMu2eHistoConsumer::stopProcessingData(void)
+	{
+		std::cout<<"[In DQMMu2eHistoConsumer () ] Stopping ..."<<std::endl;
+	
+		DataConsumer::stopProcessingData();
+		if(saveFile_)
+		{
+			std::cout<<"[In DQMMu2eHistoConsumer () ] Saving ..."<<std::endl;
+			DQMHistosBase::save();
+		}
+		closeFile();
+	}
+
+//========================================================================================================================
+	void DQMMu2eHistoConsumer::pauseProcessingData(void)
+	{
+		std::cout<<"[In DQMMu2eHistoConsumer () ] Pausing ..."<<std::endl;
+		DataConsumer::stopProcessingData();
+	}
+
+//========================================================================================================================
+	void DQMMu2eHistoConsumer::resumeProcessingData(void)
+	{
+		std::cout<<"[In DQMMu2eHistoConsumer () ] Resuming ..."<<std::endl;
+		DataConsumer::startProcessingData("");
+	}
+
+//========================================================================================================================
+	bool DQMMu2eHistoConsumer::workLoopThread(toolbox::task::WorkLoop* workLoop)
+	{
+		// std::cout<<"[In DQMMu2eHistoConsumer () ] CallingFastRead ..."<<std::endl;
+		fastRead();
+		return WorkLoop::continueWorkLoop_;
+	}
+
+//========================================================================================================================
+	void DQMMu2eHistoConsumer::fastRead(void)
+	{
+		// std::cout<<"[In DQMMu2eHistoConsumer () ] FastRead ..."<<std::endl;
+		if(DataConsumer::read(dataP_, headerP_) < 0)
+		{
+			std::this_thread::sleep_for(std::chrono::milliseconds(5000));//10
+			return;
+		}
+		double value = 0;
+		memcpy(&value, &dataP_->at(0), sizeof(double));
+	//	std::cout << __PRETTY_FUNCTION__ << " Value: " << value << std::endl;
+		// std::cout<<"[In DQMMu2eHistoConsumer () ] Filling ..."<<std::endl;
+		hGauss_->Fill(value);
+		DataConsumer::setReadSubBuffer<std::string, std::map<std::string, std::string>>();
+	}
 
 DEFINE_OTS_PROCESSOR(DQMMu2eHistoConsumer)
